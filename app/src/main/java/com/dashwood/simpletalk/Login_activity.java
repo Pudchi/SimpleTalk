ad7a186d5d4b81da63e700e7ff4a3db4dd56d949
Dashwood <pudchi330@gmail.com>
2017-02-23 15:36:22 +0800

    Firebase Login Auth

--- app/src/main/java/com/dashwood/simpletalk/Login_activity.java
+++ app/src/main/java/com/dashwood/simpletalk/Login_activity.java
@@ -6,60 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ProgressBar;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Login_activity extends AppCompatActivity {
 
-
-    EditText email, passwd;
-    Button login;
-
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private Button btnSignup, btnLogin, btnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activity);
+        setContentView(R.layout.activity_login);
 
-        email = (EditText)findViewById(R.id.login_email);
-        passwd = (EditText)findViewById(R.id.login_passwd);
-        login = (Button)findViewById(R.id.login_button);
-        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
 
-        auth.signInWithEmailAndPassword(email.getText().toString(), passwd.getText().toString())
-                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        setResult(RESULT_OK);
-                        finish();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        login.setError("登入失敗, 請檢查帳號密碼");
-                    }
-                });
+        if (auth.getCurrentUser() != null){
 
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode)
-        {
-            case 1:
-                if (resultCode != RESULT_OK)
-                {
-                    finish();
-                }
-                break;
         }
+
     }
-
-
 }
